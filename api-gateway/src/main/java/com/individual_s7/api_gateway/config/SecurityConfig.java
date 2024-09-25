fbc6b70dc1d8591446117d035475e4fbf070c306
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-09-25 23:49:06 +0200

    applied-route-to-user-service-and-auth

---
+++ api-gateway/src/main/java/com/individual_s7/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.individual_s7.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorize -> authorize
+                        .pathMatchers(HttpMethod.POST, "/**").permitAll()
+                        .anyExchange().permitAll()
+                );
+        return http.build();
+    }
+}
