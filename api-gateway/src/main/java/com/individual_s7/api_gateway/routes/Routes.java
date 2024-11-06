3ec3581d2dc8a8af8b81a075d66f31bd23071250
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-11-06 11:14:11 +0100

    updated-routes

--- api-gateway/src/main/java/com/individual_s7/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/individual_s7/api_gateway/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.individual_s7.api_gateway.routes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,17 @@ import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyReques
 
 @Configuration
 public class Routes {
+    @Value("${user.service.url}")
+    private String userServiceUrl;
+
+    @Value("${friendship.service.url}")
+    private String friendshipServiceUrl;
+
+    @Value("${post.service.url}")
+    private String postServiceUrl;
+
+    @Value("${notification.service.url}")
+    private String notificationServiceUrl;
 
     @Bean
     public RouteLocator userServiceRoutes(RouteLocatorBuilder builder) {
@@ -16,27 +28,24 @@ public class Routes {
                 // Route for user registration that bypasses authentication
                 .route("user-register", r -> r
                         .path("/api/user/register")
-                        .uri("http://localhost:8080")
+                        .uri(userServiceUrl)
                 )
                 // Route for authenticated user service calls
                 .route("user-service", r -> r
                         .path("/api/user/**")  // Match user service paths
-                        // We don't remove the Authorization header
-                        .uri("http://localhost:8080")  // Forward to the User Service URI
+                        .uri(userServiceUrl)  // Forward to the User Service URI
                 )
                 .route("friendship-service", r -> r
-                        .path("/api/friendship/**")  // Match user service paths
-                        // We don't remove the Authorization header
-                        .uri("http://localhost:8085")  // Forward to the User Service URI
+                        .path("/api/friendship/**")  // Match friendship service paths
+                        .uri(friendshipServiceUrl)  // Forward to the Friendship Service URI
                 )
                 .route("post-service", r -> r
                         .path("/api/post/**")  // Match post service paths
-                        // We don't remove the Authorization header
-                        .uri("http://localhost:8081")  // Forward to the Post Service URI
+                        .uri(postServiceUrl)  // Forward to the Post Service URI
                 )
                 .route("notification-service", r -> r
                         .path("/ws/**")
-                        .uri("ws://localhost:8083")) // Point to your notification service
+                        .uri(notificationServiceUrl)) // Forward to the Notification Service URI (WebSocket)
                 .build();
     }
 
