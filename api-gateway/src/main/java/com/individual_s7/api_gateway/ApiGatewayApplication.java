2bc7b1259224257dd4477e74b46d8bf08bf66c76
Mohammed Bouali <Med.bouali96@hotmail.com>
2024-11-06 10:55:01 +0100

    updates-env

--- api-gateway/src/main/java/com/individual_s7/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/individual_s7/api_gateway/ApiGatewayApplication.java
@@ -1,11 +1,15 @@
 package com.individual_s7.api_gateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
